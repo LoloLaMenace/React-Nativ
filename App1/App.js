701bf7e54c7c82c9e59d17f69c8a7c9f5bf5e021
Lorick <lorick.vacher17@gmail.com>
2023-12-08 10:32:56 +0100

    App1 add

--- App1/App.js
+++ App1/App.js
@@ -1,55 +1,11 @@
-import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [objectifs, setObjectifs] = useState([
-    "Faire les courses",
-    "Aller à la salle de sport 3 fois par semaine",
-    "Monter à plus de 5000m d'altitude",
-    "Acheter mon premier appartement"
-  ]);
-
-  const handleAddObjectif = () => {
-    if (inputValue === '') {
-      return;
-    }
-    setObjectifs((prevObjectifs) => [...prevObjectifs, inputValue]);
-    setInputValue('');
-  };
-
-  const handleRemoveObjectif = (index) => {
-    const updatedObjectifs = [...objectifs];
-    updatedObjectifs.splice(index, 1);
-    setObjectifs(updatedObjectifs);
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.container2}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter vos objectifs"
-          value={inputValue}
-          onChangeText={(text) => setInputValue(text)}
-        />
-        <Pressable style={styles.button} onPress={handleAddObjectif}>
-          <Text style={styles.Wtext}>AJOUTER</Text>
-        </Pressable>
-      </View>
-
-      <View style={styles.container}>
-        {objectifs.map((objectif, index) => (
-          <View key={index} style={styles.objectifContainer}>
-            <Text style={styles.textstyle}>{objectif}</Text>
-            <Pressable onPress={() => handleRemoveObjectif(index)}>
-              <Text style={[styles.deleteButton, styles.textstyle]}>❌</Text>
-            </Pressable>
-          </View>
-        ))}
-        <StatusBar style="auto" />
-      </View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -59,46 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    marginTop: 100
-  },
-  Wtext: {
-    color: 'white'
-  },
-  container2: {
-    width: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    marginBottom: 100,
-  },
-  input: {
-    height: 50,
-    width: '90%',
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: 'grey',
-    borderRadius: 150,
-    padding: 10,
-  },
-  button: {
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 150,
-    backgroundColor: 'green',
-    color: 'white'
-  },
-  objectifContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 8,
-    fontSize: '150px'
-  },
-  textstyle: {
-    fontSize: 20,
-    marginBottom: 10
-  },
-  deleteButton: {
-    color: 'red',
-    marginLeft: 10,
+    justifyContent: 'center',
   },
 });
