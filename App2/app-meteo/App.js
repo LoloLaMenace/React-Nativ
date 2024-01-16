2a468ee001859f791379643b1895195f3c09f5ad
Lorick <lorick.vacher17@gmail.com>
2024-01-16 18:00:22 +0100

    [ADD] list 24 dernier heure

--- App2/app-meteo/App.js
+++ App2/app-meteo/App.js
@@ -1,99 +1,120 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, ImageBackground, Image } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground, ActivityIndicator, Image } from 'react-native';
 import * as Location from 'expo-location';
-import apiCallComponent from './Components/apiCallComponent';
+import { format } from 'date-fns';
+import CityLocation from './Components/CityLocationComponent';
+import { FlatList } from 'react-native';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [dataDays, setDataDays] = useState(null);
 
   useEffect(() => {
-    (async () => {
+    const fetchData = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
+        setLoading(false);
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      try {
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
 
-      // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=fa1a67d96cbb561d5f26fb34bd433537&units=metric&lang=fr`;
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=fa1a67d96cbb561d5f26fb34bd433537&units=metric&lang=fr`;
 
-      // fetch(apiUrl)
-      //   .then(response => response.json())
-      //   .then(data => setWeatherData(data))
-      //   .catch(error => console.error('Error fetching weather data:', error));
-      
-      console.log(data);
-    
-    })();
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error('Error fetching current weather data:', error);
+      }
+
+      try {
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+
+        const apiUrl2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=fa1a67d96cbb561d5f26fb34bd433537&units=metric&lang=fr`;
+
+        const response2 = await fetch(apiUrl2);
+        const data2 = await response2.json();
+
+        // Afficher les 8 premiers éléments
+        const first8Forecast = data2.list.slice(0, 8);
+        setDataDays(first8Forecast);
+      } catch (error) {
+        console.error('Error fetching weather forecast data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  useEffect(() => {
-
-    if (location != null)  {
-    const data = apiCallComponent(location.coords.longitude, location.coords.latitude);
-    setWeatherData(data);
-  }
-
-  },[location])
-
-  let text = 'Waiting...';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (weatherData) {
-    const temperature = Math.round(weatherData.main.temp);
-    const weatherDescription = weatherData.weather[0].description;
-    const weatherIcon = weatherData.weather[0].icon;
-    const city = weatherData.name;
-    const tempMax = Math.round(weatherData.main.temp_max);
-    const tempMin = Math.round(weatherData.main.temp_min);
-
+  if (loading) {
     return (
-      <ImageBackground
-        source={require('./assets/back.jpg')}
-        style={styles.container}
-      >
-        <View style={styles.container1}>
-          <View style={styles.locationContainer}>
-            <View style={styles.cityContainer}>
-              <Text style={styles.cityText}>{city}</Text>
-            </View>
-            <View style={styles.cityContainer}>
-              <Text style={styles.locationText}>Ma position</Text>
-            </View>
-          </View>
-          <View style={styles.weatherInfoContainer}>
-            <Text style={styles.temperatureText}>{`${temperature}°C`}</Text>
-            {/* <Image
-              source={{ uri: `https://openweathermap.org/img/w/${weatherIcon}.png` }}
-              style={styles.weatherIcon}
-            /> */}
-          </View>
-          <View style={styles.cityContainer}>
-            <Text style={styles.cityText}>{weatherDescription}</Text>
-          </View>
-          <View style={styles.tempMinMaxContainer}>
-            <Text style={styles.tempMinMaxText}>{`↑ ${tempMax}°C`}</Text>
-            <Text style={styles.tempMinMaxText}>{`↓ ${tempMin}°C`}</Text>
-          </View>
+      <ImageBackground source={require('./assets/back.jpg')} style={styles.container}>
+        <View style={[styles.container, styles.loadingContainer]}>
+          <ActivityIndicator size="large" color="white" />
         </View>
       </ImageBackground>
     );
   }
 
-  return (
-    <ImageBackground
-      source={require('./assets/back.jpg')}
-      style={styles.container}
-    >
-      <View style={styles.container1}>
-        <Text style={styles.text}>{text}</Text>
-      </View>
-    </ImageBackground>
-  );
+  if (errorMsg) {
+    return (
+      <ImageBackground source={require('./assets/back.jpg')} style={styles.container}>
+        <View style={styles.container1}>
+          <Text style={styles.textPrevi}>{errorMsg}</Text>
+        </View>
+      </ImageBackground>
+    );
+  } else if (weatherData) {
+    const temperature = Math.round(weatherData.main.temp);
+    const weatherDescription = weatherData.weather[0].description;
+    const city = weatherData.name;
+    const tempMax = Math.round(weatherData.main.temp_max);
+    const tempMin = Math.round(weatherData.main.temp_min);
+
+    return (
+      <ImageBackground source={require('./assets/back.jpg')} style={styles.container}>
+        <CityLocation
+          city={city}
+          temperature={temperature}
+          weatherDescription={weatherDescription}
+          tempMax={tempMax}
+          tempMin={tempMin}
+        />
+
+        <View style={styles.container1}>
+          <Text style={styles.textPrevi}>{`🕒 PREVISION DES 24H DERNIERE HEURE`}</Text>
+
+          {/* FlatList pour le défilement horizontal */}
+          <FlatList
+            data={dataDays}
+            horizontal
+            keyExtractor={(item) => item.dt.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.forecastItem}>
+                <Text style={styles.forecastTime}>{format(new Date(item.dt * 1000), 'HH:mm')}</Text>
+                <Image source={{ uri: `https://openweathermap.org/img/w/${item.weather[0].icon}.png` }} style={styles.weatherIcon} />
+                <Text style={styles.forecastTemperature}>{`${Math.round(item.main.temp)}°C`}</Text>
+              </View>
+            )}
+          />
+        </View>
+        <View style={styles.container1}>
+          <Text style={styles.textPrevi}>{`🗓 PREVISION SUR 5 JOURS`}</Text>
+          
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -101,49 +122,40 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container1: {
-    marginTop: 50,
-    alignItems: 'center',
+    marginTop: 20,
     marginLeft: 10,
     marginRight: 10,
+    backgroundColor: 'rgba(62, 68, 89, 0.5)',
+    borderRadius: 10,
   },
-  text: {
-    color: 'white',
+  textPrevi: {
+    marginTop: 5,
+    marginLeft: 5,
+    marginBottom: 5,
+    color: 'rgba(250, 250, 255, 0.5)',
   },
-  temperatureText: {
+  loadingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  forecastItem: {
+    margin: 7,
+    padding: 7,
+    alignItems: 'center',
+  },
+  forecastTime: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
     color: 'white',
-    fontSize: 40,
   },
   weatherIcon: {
     width: 50,
     height: 50,
+    marginBottom: 5,
   },
-  locationContainer: {
-    marginVertical: 10,
-  },
-  locationText: {
-    color: 'white',
-    fontSize: 18,
-    alignItems: 'center',
-
-  },
-  weatherInfoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  cityContainer: {
-    marginVertical: 10,
-    alignItems: 'center',
-  },
-  cityText: {
-    color: 'white',
-    fontSize: 24,
-  },
-  tempMinMaxContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  tempMinMaxText: {
-    color: 'white',
+  forecastTemperature: {
     fontSize: 16,
+    color: 'white',
   },
 });
